6484d2975b258a6cf5a04460f95daba21f3a426f
Damian-Sanchez-Z <dsanchezzarfino@frba.utn.edu.ar>
2024-04-20 00:12:47 +0000

    main kernel changes

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -3,15 +3,6 @@
 #include <utils/hello.h>
 
 int main(int argc, char* argv[]) {
-    
-    iniciar_logger_cpu();
-
-    char* config_path = argv[1];
-
-    if(iniciar_config_cpu(config_path) == FAILURE) 
-        return EXIT_FAILURE;
-
-    iniciar_servidor_cpu();
-
-    return EXIT_SUCCESS;
+    decir_hola("Kernel");
+    return 0;
 }
