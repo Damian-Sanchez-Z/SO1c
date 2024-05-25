9b69c4727538c062f03245f38838ca11dd8cf5bf
SantiagoSugai <santiagosugai@gmail.com>
2024-05-25 17:07:06 +0000

    update errores

--- memoria/src/instrucciones.c
+++ memoria/src/instrucciones.c
@@ -57,23 +57,15 @@ void abrir_archivo_y_enviar_instrucciones(char* path){
 
 void cargar_y_enviar_instrucciones(char* vector_instrucciones, int posicion_caracter_fin){
 
-    char* codop;
-
-    char* operando;
-
     OPERACION* operacion;
 
     char* stream;
 
-    stream = (char*) malloc(70 * sizeof(char*));
+    stream = (char*) malloc(70 * sizeof(char));
 
     operacion = (OPERACION*) malloc(sizeof(OPERACION));
 
     vector_instrucciones = malloc(posicion_caracter_fin * sizeof(char));
-
-    codop = (char*) malloc(sizeof(char) * 20);
-
-    operando = (char*) malloc(sizeof(char) * 50);
 
     int contador_operando = 0;
 
@@ -88,26 +80,26 @@ void cargar_y_enviar_instrucciones(char* vector_instrucciones, int posicion_cara
             // i++;                               si quiero o no mandar el char espacio ' ' (con i++ tomo el siguiente caracter)
 
             do{
-                (asignar_a_operando(operacion, operando, contador_operando));
+                (asignar_a_operando(operacion, vector_instrucciones[i], contador_operando));
 
                 i++;
 
                 contador_operando ++;
 
-            } while(vector_instrucciones[i] != '\n');
+            } while(vector_instrucciones[i+1] != '\n');
 
 
         }
 
         if(vector_instrucciones[i] == '\n'){
 
-            operando[i+1] = '\0';
+            operacion->operando_de_operacion[i+1] = '\0';
 
             eliminar_paquete(paquete);
 
             paquete = crear_paquete();
 
-            crear_stream(stream, codop, operando);
+            crear_stream(stream, operacion->codigo_de_operacion, operacion->operando_de_operacion);
             
             agregar_a_paquete(paquete, stream, sizeof(stream));
 
@@ -123,9 +115,9 @@ void cargar_y_enviar_instrucciones(char* vector_instrucciones, int posicion_cara
 
             vaciar_string(stream, i);
 
-            vaciar_string(codop, i);
+            vaciar_string(operacion->codigo_de_operacion, i);
 
-            vaciar_string(operando, i);
+            vaciar_string(operacion->operando_de_operacion, i);
 
             i++;
 
@@ -135,15 +127,17 @@ void cargar_y_enviar_instrucciones(char* vector_instrucciones, int posicion_cara
 
         }
 
-        asignar_a_codop(operacion, codop, contador_codop);
+        asignar_a_codop(operacion, vector_instrucciones[i], contador_codop);
 
         contador_codop ++;        
 
     }
 
-    free(codop);
+    //wait(llegue pedido);
 
-    free(operando);
+    //enviar_mensaje("envio_finalizado", socket);
+
+    printf("Envio de instrucciones finalizado");
 
     free(vector_instrucciones);
 
@@ -152,15 +146,15 @@ void cargar_y_enviar_instrucciones(char* vector_instrucciones, int posicion_cara
     free(operacion);
 }
 
-void asignar_a_codop(OPERACION* operacion, char codop, int contador){
+void asignar_a_codop(OPERACION* operacion, char caracter_codop, int contador){
 
-    operacion->codigo_de_operacion[contador] = codop;
+    operacion->codigo_de_operacion[contador] = caracter_codop;
 
 }
 
-void asignar_a_operando(OPERACION* operacion, char operando, int contador){
+void asignar_a_operando(OPERACION* operacion, char caracter_operando, int contador){
 
-    operacion->operando_de_operacion[contador] = operando;
+    operacion->operando_de_operacion[contador] = caracter_operando;
 
 }
 
@@ -179,7 +173,6 @@ void vaciar_string(char* string, int ultimo_lugar){
     }
 
 }
-
 
 void retardo_milisegundos(int milisegundos){
 
